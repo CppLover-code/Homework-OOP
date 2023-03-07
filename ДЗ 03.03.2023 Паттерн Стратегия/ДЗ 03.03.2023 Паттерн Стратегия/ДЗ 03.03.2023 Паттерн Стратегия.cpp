f05e293e24810f76e5a03af2418df35b985d1f5e
Anna Boiko <annaboiko377@gmail.com>
2023-03-07 13:42:32 +0300

    !

---
+++ ДЗ 03.03.2023 Паттерн Стратегия/ДЗ 03.03.2023 Паттерн Стратегия/ДЗ 03.03.2023 Паттерн Стратегия.cpp
@@ -0,0 +1,80 @@
+﻿// Реализовать паттерн "Стратегия" на основе примера, приведенного в презентации.
+// Применить две стратегии сортировки - по возрастанию и по убыванию.
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class SortStrategy abstract
+{
+public:
+    virtual void Sort(vector<string>& v) abstract;
+};
+
+class Descending : public SortStrategy          // сортировка по убыванию
+{
+    void Sort(vector<string>& v) override
+    {
+        sort(v.begin(), v.end(), greater<>());
+    }
+};
+
+class Ascending : public SortStrategy           // сортировка по возрастанию
+{
+    void Sort(vector<string>& v) override
+    {
+        sort(v.begin(), v.end(), less<>());
+    }
+};
+
+class SortedArray
+{
+    vector<string> vector;
+    SortStrategy* sortstrategy{};
+
+public:
+    void SetSortStrategy(SortStrategy* sortstrategy)
+    {
+        this->sortstrategy = sortstrategy;
+    }
+    void Add(string name)
+    {
+        vector.push_back(name);
+    }
+    void Sort()
+    {
+        sortstrategy->Sort(vector);
+
+        for (string name : vector)
+        {
+            cout << " " + name << endl;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    SortedArray ar;
+
+    ar.Add("Strawberry");
+    ar.Add("Apple");
+    ar.Add("Pineapple");
+    ar.Add("Watermelon");
+    ar.Add("Grape");
+    ar.Add("Apricot");
+    ar.Add("Plum");
+    ar.Add("Banana");
+    ar.Add("Orange");
+    ar.Add("Pear");
+
+    Ascending sortUp;
+    ar.SetSortStrategy(&sortUp);
+    ar.Sort();
+
+    Descending sortDown;
+    ar.SetSortStrategy(&sortDown);
+    ar.Sort();
+}
